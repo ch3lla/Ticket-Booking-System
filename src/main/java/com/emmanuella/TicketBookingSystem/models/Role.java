8575905e24bb1588ade73278050c74edadffa9fb
ch3lla <chukwumaemmanuella03@gmail.com>
2023-08-12 15:23:26 +0100

    adding roles and user authentication

---
+++ src/main/java/com/emmanuella/TicketBookingSystem/models/Role.java
@@ -0,0 +1,19 @@
+package com.emmanuella.TicketBookingSystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
---
+++ src/main/java/com/emmanuella/TicketBookingSystem/models/User.java
@@ -0,0 +1,45 @@
+package com.emmanuella.TicketBookingSystem.models;
+
+import com.emmanuella.TicketBookingSystem.dto.UserDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String email, String password){
+        email = this.email;
+        password = this.password;
+    }
+    public User(String username, String password, String email){
+        username = this.username;
+        email = this.email;
+        password = this.password;
+    }
+
+    public User(UserDto userDto){
+        if (userDto.getUserName() != null){
+            this.username = userDto.getUserName();
+        }
+        if (userDto.getPassword() != null){
+            this.username = userDto.getPassword();
+        }
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
+
+
+}
